3ae5a99f675dafb7930a499eb209b37404a7f897
Zachary Stevens <zts@cryptocracy.com>
2013-11-02 22:53:38 +0000

    DRY up aws-cloudformation plugin.

--- files/default/ohai_plugins/aws-cloudformation.rb
+++ files/default/ohai_plugins/aws-cloudformation.rb
@@ -4,31 +4,14 @@ require_plugin "ec2"
 
 aws[:cloudformation] = Mash.new unless aws[:cloudformation]
 
-ec2_conn = AWS::EC2.new
-instance = ec2_conn.instances[ec2[:instance_id]]
-tags = instance.tags
-
-aws[:cloudformation][:stack_name] = tags["aws:cloudformation:stack-name"]
+# Get the name and logical id of the stack this instance is part of
+tags = AWS::EC2.new.instances[ec2[:instance_id]].tags
+stack_name = aws[:cloudformation][:stack_name] = tags["aws:cloudformation:stack-name"]
 aws[:cloudformation][:logical_id] = tags["aws:cloudformation:logical-id"]
 
+# Get the details of every stack we can see
 cfn_conn = AWS::CloudFormation.new
-stack_name = tags["aws:cloudformation:stack-name"]
 stack = cfn_conn.stacks[stack_name]
-
-aws[:cloudformation][:parameters] = stack.parameters
-aws[:cloudformation][:resources] ||= Mash.new
-stack.resource_summaries.each do |rs|
-  resource = {
-    :type          => rs[:resource_type],
-    :logical_id    => rs[:logical_resource_id],
-    :physical_id   => rs[:physical_resource_id],
-    :status        => rs[:resource_status],
-    :status_reason => rs[:resource_status_reason],
-    :last_updated  => rs[:last_updated_timestamp],
-  }
-  id = rs[:logical_resource_id]
-  aws[:cloudformation][:resources][id] = resource
-end
 
 aws[:cloudformation][:stacks] ||= Mash.new
 stack = cfn_conn.stacks.each do |stack|
@@ -49,3 +32,7 @@ stack = cfn_conn.stacks.each do |stack|
     aws[:cloudformation][:stacks][stack.name][:resources][id] = resource
   end
 end
+
+# For backwards compatibility, copy this stack's params and resources
+aws[:cloudformation][:parameters] = aws[:cloudformation][:stacks][stack_name][:parameters]
+aws[:cloudformation][:resources] = aws[:cloudformation][:stacks][stack_name][:resources]
